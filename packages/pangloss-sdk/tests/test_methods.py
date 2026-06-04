6c13793bca28088c91dd69b8e520f6243b36cd5e
Richard Hadden <rwjhadden@gmail.com>
2026-06-04 14:21:33 +0200

    module testing running ok

--- packages/pangloss-sdk/tests/test_methods.py
+++ packages/pangloss-sdk/tests/test_methods.py
@@ -1,15 +1,23 @@
+from typing import no_type_check
 from uuid import uuid7
 
 from pangloss_models import initialise
 from pangloss_models.model_bases.document import Document
 
 
+@no_type_check
 def test_get_item():
+    """Currently tests a hard-coded return type to make sure database module bindings work"""
+
     class Statement(Document):
         pass
 
     initialise()
 
-    print(Statement.get(id=uuid7()))
+    st = Statement.get(id=uuid7())
 
-    assert False
+    assert isinstance(st, Statement.HeadView)
+
+    assert st.id
+    assert st.label == "A document"
+    assert st.type == "Statement"
